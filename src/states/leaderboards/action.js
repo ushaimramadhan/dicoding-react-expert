2fac695a7f9e600c536c7c195efa01f436aa0378
Ushaim Ramadhan F <adhan1005@gmail.com>
2026-02-01 20:48:56 +0700

    create leaderboards action & reducer

---
+++ src/states/leaderboards/action.js
@@ -0,0 +1,29 @@
+import api from '../../utils/api';
+
+const ActionType = {
+  RECEIVE_LEADERBOARDS: 'RECEIVE_LEADERBOARDS',
+};
+
+function receiveLeaderboardsActionCreator(leaderboards) {
+  return {
+    type: ActionType.RECEIVE_LEADERBOARDS,
+    payload: { leaderboards },
+  };
+}
+
+function asyncReceiveLeaderboards() {
+  return async (dispatch) => {
+    try {
+      const leaderboards = await api.getLeaderboards();
+      dispatch(receiveLeaderboardsActionCreator(leaderboards));
+    } catch (error) {
+      alert(error.message);
+    }
+  };
+}
+
+export {
+  ActionType,
+  receiveLeaderboardsActionCreator,
+  asyncReceiveLeaderboards,
+};
